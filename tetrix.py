8558661d1df231a1787a68c6d4acb1d3e398b26c
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-04-30 14:16:50 -0300

    created a Piece Enum to list possible pieces of the game

--- tetrix.py
+++ tetrix.py
@@ -1,1 +1,12 @@
-print('initial commit')
+from enum import IntEnum
+
+
+class Piece(IntEnum):
+    NoShape = 0
+    ZShape = 1
+    SShape = 2
+    LineShape = 3
+    TShape = 4
+    SquareShape = 5
+    LShape = 6
+    MirroredLShape = 7
